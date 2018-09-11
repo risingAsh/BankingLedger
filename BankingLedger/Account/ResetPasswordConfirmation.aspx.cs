32759c6b3c59dcea31fd1e1f27382e23b3385887
Kenneth Martin <ktm064@gmail.com>
2018-09-10 20:48:51 -0700

    Add project files.

---
+++ BankingLedger/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace BankingLedger.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ BankingLedger/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BankingLedger.Startup))]
+namespace BankingLedger
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
